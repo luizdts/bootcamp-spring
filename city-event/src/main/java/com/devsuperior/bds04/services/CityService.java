95590ec4509ba28f2b57297968b3a2229fcac480
Luiz Henrique Araújo Dantas <luizhadantas@gmail.com>
2022-10-16 00:53:58 -0300

    Adicionando projetos com validação e autenticação

---
+++ city-event/src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,34 @@
+package com.devsuperior.bds04.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.repositories.CityRepository;
+
+@Service
+public class CityService {
+
+	@Autowired
+	private CityRepository repository;
+	
+	public List<CityDTO> findAll() {
+		List<City> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public CityDTO insert(CityDTO cityDTO) {
+		City entity = new City();
+		entity.setName(cityDTO.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+	}
+}
